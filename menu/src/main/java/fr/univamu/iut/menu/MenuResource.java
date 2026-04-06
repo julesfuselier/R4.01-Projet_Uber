079460cda96e2ca5a64b5804245ae8f2fb0cf3ea
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2026-04-06 18:03:50 +0200

    simple changment au niveau des imports

--- menu/src/main/java/fr/univamu/iut/menu/MenuResource.java
+++ menu/src/main/java/fr/univamu/iut/menu/MenuResource.java
@@ -4,11 +4,14 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 
+import java.util.ArrayList;
+
 @Path("/hello-world")
-public class HelloResource {
+public class MenuResource {
+
     @GET
     @Produces("text/plain")
     public String hello() {
-        return "Hello, World!";
+        return "Hello world";
     }
 }
