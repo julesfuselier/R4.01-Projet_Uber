50b5ef3212be828a5d8e38ac93d045fd7f690ee5
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2026-04-06 18:02:58 +0200

    feat : Création de la classe métier gérant les plats

--- menu/src/main/java/fr/univamu/iut/menu/PlatResume.java
+++ menu/src/main/java/fr/univamu/iut/menu/PlatResume.java
@@ -1,4 +1,41 @@
 package fr.univamu.iut.menu;
 
 public class PlatResume {
+
+    /** Dish's id **/
+    private int id;
+
+    /** Dish's name **/
+    private String name;
+
+    /** Dish's price **/
+    private double price;
+
+    public PlatResume() {}
+
+    /** Dish's constructor **/
+    public PlatResume(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    /** All dish's getters **/
+    public int getId() { return this.id; }
+    public String getName() { return this.name; }
+    public double getPrice() { return this.price; }
+
+    /** All dish's setters **/
+    public void setId(int newId) { this.id = newId; }
+    public void setName(String newName) { this.name = newName; }
+    public void setPrice(double newPrice) {this.price = newPrice; }
+
+    /** Dish's to string **/
+    public String toString() {
+        return "Plat{" +
+                "id='" + this.id + '\'' +
+                ", nom='" + this.name + '\'' +
+                ", prix='" + this.price + '\'' +
+                '}';
+    }
 }
