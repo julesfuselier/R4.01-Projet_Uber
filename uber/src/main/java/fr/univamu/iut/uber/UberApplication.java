4fe97f363670efbdc8ebd95e9d37d339465dbebc
Loan <loan.allard@etu.univ-amu.fr>
2026-04-08 11:34:46 +0200

    Add UberApplication class and initialize database with user and dish data

---
+++ uber/src/main/java/fr/univamu/iut/uber/UberApplication.java
@@ -0,0 +1,8 @@
+package fr.univamu.iut.uber;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class UberApplication extends Application {
+}
