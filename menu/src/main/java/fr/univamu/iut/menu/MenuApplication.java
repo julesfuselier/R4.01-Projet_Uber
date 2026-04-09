9f8b51e991f052c28f2aec64c0fa99b28fb9796b
nularyNasser <nahamed.sveil@gmail.com>
2026-04-09 16:29:39 +0200

    plus de .env

--- menu/src/main/java/fr/univamu/iut/menu/MenuApplication.java
+++ menu/src/main/java/fr/univamu/iut/menu/MenuApplication.java
@@ -13,16 +13,16 @@ import jakarta.ws.rs.core.Application;
 @ApplicationScoped
 public class MenuApplication extends Application {
 
-    Dotenv dotenv = Dotenv.load();
+//    Dotenv dotenv = Dotenv.load();
 
     @Produces
     private MenuRepositoryInterface openDbConnection() {
-        String dbUrl = dotenv.get("DB_URL");
-        String dbUser = dotenv.get("DB_USER");
-        String dbPass = dotenv.get("DB_PASS");
+//        String dbUrl = dotenv.get("DB_URL");
+//        String dbUser = dotenv.get("DB_USER");
+//        String dbPass = dotenv.get("DB_PASS");
 
         try {
-            return new MenuRepositoryMariadb(dbUrl, dbUser, dbPass);
+            return new MenuRepositoryMariadb("jdbc:mariadb://mysql-blog-td.alwaysdata.net/blog-td_menu-api", "blog-td", "Rb.velocity+6");
         } catch (Exception e) {
             throw new RuntimeException("Impossible de se connecter à la base de données", e);
         }
